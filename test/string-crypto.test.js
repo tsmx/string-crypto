36690d2e40b5280368afc2d5e83c98817825b447
tsmx <strs@freenet.de>
2020-10-11 20:44:39 +0200

    Initial commit.

---
+++ test/string-crypto.test.js
@@ -0,0 +1,103 @@
+const { expect } = require('@jest/globals');
+
+describe('secure-config test suite', () => {
+
+    const testString = 'Test123$ üöä';
+    const hexReg = new RegExp('^[0-9A-F]*$', 'i');
+    const testKeyChar = '0123456789qwertzuiopasdfghjklyxc';
+    const testKeyCharBad = '00000000000000000000000000000000';
+    const testKeyCharWrongLength = '0123456789qwertzuiopasdfghjklyxc000';
+    const testKeyHex = '9af7d400be4705147dc724db25bfd2513aa11d6013d7bf7bdb2bfe050593bd0f';
+
+    beforeEach(() => {
+        jest.resetModules();
+        delete process.env['ENCRYPTION_KEY'];
+    });
+
+    it('tests a successful encryption and decryption with char key from environment var', async (done) => {
+        process.env['ENCRYPTION_KEY'] = testKeyChar;
+        const sc = require('../string-crypto');
+        const encrypted = sc.encrypt(testString);
+        const encryptedParts = encrypted.split('|');
+        expect(encryptedParts.length).toBe(2);
+        expect(hexReg.test(encryptedParts[0])).toBeTruthy();
+        expect(hexReg.test(encryptedParts[1])).toBeTruthy();
+        const decrypted = sc.decrypt(encrypted);
+        expect(decrypted).toStrictEqual(testString);
+        done();
+    });
+
+    it('tests a successful encryption and decryption with hex key from environment var', async (done) => {
+        process.env['ENCRYPTION_KEY'] = testKeyHex;
+        const sc = require('../string-crypto');
+        const encrypted = sc.encrypt(testString);
+        const encryptedParts = encrypted.split('|');
+        expect(encryptedParts.length).toBe(2);
+        expect(hexReg.test(encryptedParts[0])).toBeTruthy();
+        expect(hexReg.test(encryptedParts[1])).toBeTruthy();
+        const decrypted = sc.decrypt(encrypted);
+        expect(decrypted).toStrictEqual(testString);
+        done();
+    });
+
+    it('tests a successful encryption and decryption with char key from options', async (done) => {
+        const options = {
+            key: testKeyChar
+        }
+        const sc = require('../string-crypto');
+        const encrypted = sc.encrypt(testString, options);
+        const encryptedParts = encrypted.split('|');
+        expect(encryptedParts.length).toBe(2);
+        expect(hexReg.test(encryptedParts[0])).toBeTruthy();
+        expect(hexReg.test(encryptedParts[1])).toBeTruthy();
+        const decrypted = sc.decrypt(encrypted, options);
+        expect(decrypted).toStrictEqual(testString);
+        done();
+    });
+
+    it('tests a successful encryption and decryption with hex key from options', async (done) => {
+        const options = {
+            key: testKeyHex
+        }
+        const sc = require('../string-crypto');
+        const encrypted = sc.encrypt(testString, options);
+        const encryptedParts = encrypted.split('|');
+        expect(encryptedParts.length).toBe(2);
+        expect(hexReg.test(encryptedParts[0])).toBeTruthy();
+        expect(hexReg.test(encryptedParts[1])).toBeTruthy();
+        const decrypted = sc.decrypt(encrypted, options);
+        expect(decrypted).toStrictEqual(testString);
+        done();
+    });
+
+    it('tests a failed encryption because of missing key', async (done) => {
+        const sc = require('../string-crypto');
+        expect(() => { sc.encrypt(testString); }).toThrow('Key not found.');
+        done();
+    });
+
+    it('tests a failed encryption because of wrong key length from environment var', async (done) => {
+        const sc = require('../string-crypto');
+        process.env['ENCRYPTION_KEY'] = testKeyCharWrongLength;
+        expect(() => { sc.encrypt(testString); }).toThrow('Key length');
+        done();
+    });
+
+    it('tests a failed encryption because of wrong key length from options', async (done) => {
+        const sc = require('../string-crypto');
+        expect(() => { sc.encrypt(testString, { key: testKeyCharWrongLength }); }).toThrow('Key length');
+        done();
+    });
+
+    it('tests a failed decryption because of a wrong key', async (done) => {
+        const sc = require('../string-crypto');
+        const encrypted = sc.encrypt(testString, { key: testKeyChar });
+        const encryptedParts = encrypted.split('|');
+        expect(encryptedParts.length).toBe(2);
+        expect(hexReg.test(encryptedParts[0])).toBeTruthy();
+        expect(hexReg.test(encryptedParts[1])).toBeTruthy();
+        expect(() => { sc.decrypt(encrypted, { key: testKeyCharBad }); }).toThrow('Decryption failed.');
+        done();
+    });
+
+});
